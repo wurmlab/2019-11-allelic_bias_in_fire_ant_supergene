e3733277e90bc51ad2bea20f381a5c535eab39d1
Carlos Martinez-Ruiz <c.martinezruiz@qmul.ac.uk>
2019-07-03 17:00:24 +0100

    Add wasp script to the analysis

--- 11-whole_genome_ase/whole_genome_ase.sh
+++ 11-whole_genome_ase/whole_genome_ase.sh
@@ -15,12 +15,21 @@ gzip -c tmp/Si_gnG_littleb.fna > input/reference.fna.gz
 #------- ALIGN THE READS TO ALTERNATIVE REFERENCE ---------------------
 #Generate a splice junction file for the alternative reference
 sh star_alignment_all_samples_south_america.sh
-#Move the splice junction file to input
-cp tmp/all_samples_south_americaSJ.out.tab input/
+#Change name of the output bam directory
+mv tmp/bam_files tmp/bam_all
+#Change name of the referecne directory
+mv tmp/star_reference_south_america tmp/star_reference
+#Link the splice junction file to input
+ln -sr tmp/bam_all/all_samples_south_americaSJ.out.tab input
 #Generate a text file with the sample names
 ls input/ar_trimmed_reads/*gz | cut -d "/"  -f3 | cut -d "R" -f1 | sort -u > tmp/samples.txt
-gunzip -c input/reference.fna.gz > tmp/reference.fna
 
 mkdir tmp/bam_files
 #Run actual alignment in an array job
 qsub star_alignment_south_america.array.sh
+#Add inputs for the next step (bam merging + WASP removal of duplicates)
+ln -sr ../../results/2018-12-03-B_align_south_american_rna_to_BigB_gnG/results/ input/bigB_bam
+ln -sr tmp/bam_files/ input/littleb_bam
+
+#Run the script for creating reference bias free bam files
+sh wasp_script.sh
