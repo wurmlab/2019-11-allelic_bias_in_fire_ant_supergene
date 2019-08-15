caf3cae15de20c45ec4fb3852cf3ec40016ac876
Carlos Martinez-Ruiz <c.martinezruiz@qmul.ac.uk>
2019-08-15 17:52:19 +0100

    Add scripts to get SNP effects per gene

--- 13-snp_effects/generate_ensembl_vcf.sh
+++ 13-snp_effects/generate_ensembl_vcf.sh
@@ -4,14 +4,16 @@ module load genometools/1.5.9
 module load python/3.6.3
 module load ruby/2.4.1
 
-#Use flo to generate a chain file between gnGA and gnG, then use it to lift over the gnGA vcf file.
+#Unzip the ensembl reference
+gunzip -c input/Si_gnG_ensembl.fa.gz > tmp/Si_gnG_ensembl.fa
+#Use flo to generate a chain file between gnG and Ensembl, then use it to lift over the gnG vcf file.
 #Flo was created by Anurag Priyam and the explanation on how to use it is here:
 #https://github.com/wurmlab/flo
 #Copy yaml file
 cp ~/software/flo/opts_example.yaml ./flo_opts.yaml
 #Modify .yaml file using a text editor, in this case, vim. Comment out akk the 'add to path' commands
-#source_fa: 'input/Si_gnGA.fa'
-#target_fa: 'input/Si_gnG.fa'
+#source_fa: 'input/Si_gnG.fa'
+#target_fa: 'input/Si_gnG_ensembl.fa'
 #processes: '24'
 #Comment out the two last lines of the yaml file, as we do not need gff outputs
 
@@ -21,9 +23,7 @@ rake -f ~/software/flo/Rakefile
 #Move the run directory into tmp
 mv run tmp/
 
-#Unzip the vcf file
-gunzip -c input/gnGA_sa.vcf.gz > tmp/gnGA_sa.vcf
 #Run CrossMap to liftover the vcf from gnGA to gnG
 source ~/crossmaps/bin/activate
-CrossMap.py vcf tmp/run/liftover.chn tmp/gnGA_sa.vcf input/Si_gnG.fa tmp/gng_sa.vcf
+CrossMap.py vcf tmp/run/liftover.chn input/subset_south_america.vcf tmp/Si_gnG_ensembl.fa tmp/ensembl_sa.vcf
 deactivate
---
+++ 13-snp_effects/snp_effects.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+module load bcftools/1.8
+module load parallel
+#This script will perform two analyses on the vcfs generated for Solenopsis invicta populations in South
+#and North America. The first analysis will use bcftools isec to determine which variants are in common between
+#the two populations.
+#The second analysis will use SNPeff on the South American VCF to check for the effect of the SNPs on the
+#genes of S.invicta
+
+#Comparison between populations
+#Compress the files for bcftools to work
+ls input/*vcf | cut -d '/' -f 2 | parallel 'bgzip -c input/{} > tmp/{}.gz' :::
+#Index files
+ls tmp/subset_*gz | parallel 'bcftools index {}' :::
+
+#Output positions present in both files
+bcftools isec -n =2 tmp/subset_north_america.vcf.gz tmp/subset_south_america.vcf.gz -o tmp/common_snps.vcf
+#Number of variants in common between the two populations
+wc -l tmp/common_snps.vcf
+#2877, ~ 92% of the SNPs in South American
+
+#Use SNPeff to detect the effect of SNPs in genes.
+#The VCF is based on gnG (from RefSeq), but SNPeff uses annotations based on the Ensembl notation.
+#Use 'flo' and CrossMap to liftover the RefSeq VCF notation to Ensembl
+sh generate_ensembl_vcf.sh
+
+#Run SNPeff on the South American vcf (made verbose by -v)
+java -jar snpEff.jar Solenopsis_invicta -v\
+          tmp/ensembl_sa.vcf > tmp/eff_supergene.vcf
