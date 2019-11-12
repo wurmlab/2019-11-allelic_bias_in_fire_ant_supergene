ba020f93c0ac3631e72dd090546c2f12bcf52320
Carlos Martinez-Ruiz <c.martinezruiz@qmul.ac.uk>
2019-11-12 16:17:21 +0000

    Add commands to detect mis-assigned variants

--- 01-subsetting_vcf/subset_vcfs.sh
+++ 01-subsetting_vcf/subset_vcfs.sh
@@ -1,6 +1,7 @@
 #Script to generate a vcf with fixed differences between SB and Sb for the supergene region only.
 module load R/3.4.3
 module load bcftools/1.8
+module load vcftools/0.1.15
 #Generate the file 'supergene_scaffolds.ids' with only the names of the scaffolds within the supergene region
 #in the gnG assembly of Solenopsis invicta.
 Rscript supergene_scaffolds.R
@@ -28,3 +29,19 @@ Rscript fixed_diffs.R
 #South America
 #grep -vc '#' results/supergene_fixed_diffs_south_america.vcf
 #3129
+
+#The vast majority of the reference variant belong to SB and the alternative, to Sb, but in a few cases this is not true.
+#Find the SNPs that are in the 'wrong' category (alternative in all SBs or reference in all Sbs)
+#In North America
+vcftools --vcf results/supergene_fixed_diffs_north_america.vcf --indv f1-bigB --extract-FORMAT-info GT --out tmp/genotypes
+grep '1$' tmp/genotypes.GT.FORMAT > tmp/misassigned_snps_na.tab
+#How many are misassigned?
+wc -l tmp/misassigned_snps_na.tab
+#125
+
+#In South America
+vcftools --vcf results/supergene_fixed_diffs_south_america.vcf --indv AR102-1-bigB-p --extract-FORMAT-info GT --out tmp/genotypes
+grep '1$' tmp/genotypes.GT.FORMAT > tmp/misassigned_snps_sa.tab
+#How many are misassigned?
+wc -l tmp/misassigned_snps_sa.tab
+#33
