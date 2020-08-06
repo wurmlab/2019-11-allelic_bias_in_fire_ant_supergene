6a804c78b336559bfda168ec16fa2e5a24176420
Carlos Martinez-Ruiz <c.martinezruiz@qmul.ac.uk>
2020-08-06 07:30:54 +0100

    Update Figure 1 and 4 scripts

--- 09-social_form_ase_analysis/AnalysisAndTests.R
+++ 09-social_form_ase_analysis/AnalysisAndTests.R
@@ -1,6 +1,6 @@
 # test and analysis of data
 
-# load north_america_supergene_expression.RData
+# load Figure 4 source data
 load(file.choose())
 
 # load AnalysisFunctions
@@ -11,7 +11,7 @@ test1 <- all.equal(antilogit2(c(-Inf,0,1000))-c(0,0.5,1),rep(0,3))
 test1
 
 # extract logit2 values and weights
-# x relative expression of locus between single and multiple-queen colonies 
+# x relative expression of locus between single and multiple-queen colonies
 # y relative expression of SB/SB alleles within multiqueen colonies
 # wts sample size for SB/Sb comparison as a weight
 
@@ -19,15 +19,17 @@ test1
  yvals <- for_richard$lfcs_ase
  wts <- for_richard$mean_reads_ase
 
-# antilogit transform  
+# antilogit transform
  transx <- antilogit2(xvals)
  transy <- antilogit2(yvals)
- 
- 
+
+
  mod1 <- lm(transy ~ transx, weights = wts)
  summary(mod1)
-abline(mod1) 
+abline(mod1)
 
+# Test deviation from (0.5,0.5)
+summary(lm(I(transy-0.5) ~ I(transx - 0.5), weights = wts))
 
 
 # Test for the subset with x<0.5 and x>0.5
@@ -43,26 +45,37 @@ predy <- predict(mod3, interval = 'predict', level = 0.95^(1/294))
 low <- transy < predy[,'lwr']
 hi <- transy > predy[,'upr']
 cols <- rep('black', length(transx))
-cols[low] <- 'blue'
-cols[hi] <- 'blue'
+#cols[low] <- 'blue'
+#cols[hi] <- 'blue'
 
 plot(transx,transy,
      cex=sqrt(wts/mean(wts)),
      main='Allele specific expression',
      ylab=expression('Proportion of SB allele (P'[B]*')'),
-     xlab=expression('Relative expression of the locus in multiple-queen colonies (P'[Sb]*')'),
-     col = cols
-)
+     xlab=expression('Relative expression of the locus in multiple-queen colonies (P'[MQ]*')'),
+     col = cols)
 
 abline(h=0.5)
 abline(v=0.5)
-abline(mod1) 
+abline(mod1, lwd = 2)
 
 # get sorted fitted values to plot
 fy <- sort(fitted(mod3), index.return = TRUE)
 # use the same sort for x values
 fx <- transx[fy$ix]
 # add relationship to the plot
-lines(fx,fy$x, col='red')
+lines(fx,fy$x, col='red', lwd = 3)
 
+# Add the curve for Sb degeneration alone
+xvals <- seq(0.2,0.9, length.out = 100)
+yvals <- (xvals - 1) / -2 / xvals
+lines(xvals, yvals, col='purple', lwd = 2)
 
+# this is equivalent to x=1/(2y +1)
+yvals <- seq(0.1, 0.9, length.out = 100)
+xvals <- 1/(2*yvals +1)
+lines(xvals, yvals, col = 'purple', lwd = 2)
+
+mod4 <- lm(transy ~ I((transx - 1) / -2 / transx), offset = (transx - 1) / -2 / transx, weights = wts)
+summary(mod4)
+anova(mod4)
